7b802bf8e94d1c55b61b9b2a1bc76b5f2d290b4d
Raedanwin <Raedanwin@gmail.com>
2020-11-02 12:21:07 -0500

    did too much, forgot to commit, i'm re-adding everything incrementally to get my commits fixed /// all files/folders added, dependencies, server.js done

--- server.js
+++ server.js
@@ -1,37 +1,55 @@
 // DEPENDENCIES
-const express = require(`express`)
-const methodOverride = require(`method-override`)
-const mongoose = require(`mongoose`)
-const session = (`express-session`)
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const session = require(`express-session`)
 
 // CONFIGURATION
 require(`dotenv`).config()
 const app = express()
 const db = mongoose.connection
 const PORT = process.env.PORT
-const mongodbURI = process.env.mongodbURI
+const mongodbURI = process.env.MONGODBURI
 
 // MIDDLEWARE
-app.use(methodOverride(`_method`))
-app.use(express.urlencoded( {extended: true} ))
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
 
 // DATABASE
 mongoose.connect(
-    mongodbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    },
-    () => {
-        console.log(`the connection with mongod is established at`, mongodbURI)
-    }
+  mongodbURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  },
+  () => {
+    console.log('the connection with mongod is established at', mongodbURI)
+  }
 )
 
 app.use(
     session({
-        key: process.env.APIKEY,
+        secret: process.env.SECRET,
         resave: false,
         saveUninitialized: false
     })
 )
 
+db.on('error', err => console.log(err.message + ' is mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+// Controllers
+const pantryController = require('./controllers/pantry_controller.js')
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.redirect('/pantry')
+})
+app.use('/pantry', pantryController)
+
+// Listener
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT)
+})
