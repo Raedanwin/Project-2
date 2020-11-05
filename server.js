7bc985064032c2dd743e7fd67b445ec88ac561a8
Raedanwin <Raedanwin@gmail.com>
2020-11-04 20:46:54 -0500

    search returning things

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const session = require(`express-session`)
-
+const fetch = require("node-fetch");
 // CONFIGURATION
 require(`dotenv`).config()
 const app = express()
 const db = mongoose.connection
 const PORT = process.env.PORT
 const mongodbURI = process.env.MONGODBURI
+const secret = process.env.SECRET
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
@@ -44,6 +45,7 @@ db.on('disconnected', () => console.log('mongo disconnected'))
 const pantryController = require('./controllers/pantry_controller.js')
 
 const recipeController = require(`./controllers/recipe_controller.js`)
+const { response } = require('express')
 
 
 // Routes
@@ -57,6 +59,24 @@ app.get(`/`, (req, res) => {
 })
 app.use(`/recipes`, recipeController)
 
+// SEARCH ROUTES (will move later)
+app.get(`/search`, (req, res) => {
+  res.render(`../views/search.ejs`)
+})
+
+app.post(`/search`, (req, res) => {
+  let search = req.body.search
+  
+  // fetch(route)
+  // .then(response => response.json())
+  // .then(data => console.log(data));
+function getSearch(){
+  let route = "https://api.spoonacular.com/recipes/complexSearch?query="+search+"&apiKey=74773f7220eb40d08a371c9c65b48a6d"
+  fetch(route).then(response =>{return response.json()}).then(data=>{res.render(`../views/resultPage.ejs`,{banana:JSON.stringify(data.results)})}).catch(err=>{console.log(err)})
+}
+getSearch()
+
+})
 // Listener
 app.listen(PORT, () => {
   console.log('Listening on port', PORT)
